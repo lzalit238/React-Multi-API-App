58167743279ffe1c3beb916a354aa2782aaaea48
lzalit238 <lzalit238@west-mec.org>
2021-10-06 09:25:18 -0700

    Axios

--- multi-api-react-app/src/App.jsx
+++ multi-api-react-app/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import ParksCards from './Components/ParksCards';
 
 function App() {
+  const axios = require('axios').default
+    axios.get('https://developer.nps.gov/api/v1/parks?&api_key=vcEt1EQ8P1p32S4YDuRlHUhUFtuNvYyew2t2xnnJ').then((response) => {
+      console.log(response);
+    }).catch((error) => {
+    console.log(error)
+  })
 
   return (
     <div className="App">
       <h1>Welcome!</h1>
       <h3>Basic React Template</h3>
+      <ParksCards />
     </div>
   );
 }
--- multi-api-react-app/src/Components/ParksCards.jsx
+++ multi-api-react-app/src/Components/ParksCards.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 const url = 'https://developer.nps.gov/api/v1/parks?&api_key=vcEt1EQ8P1p32S4YDuRlHUhUFtuNvYyew2t2xnnJ'
 
 const ParksCards = ({ data }) => {
-    const {id, url, parkCode, description, latLong, activities, contacts, directionsURL} = data
+    // const {id, url, parkCode, description, latLong, activities, contacts, directionsURL} = data
     return (
-        <div>
-            
-        </div>
+        <>
+            {/* <div key={id}>
+                <div>{description}</div>
+            </div> */}
+        </>
     )
 }
 
