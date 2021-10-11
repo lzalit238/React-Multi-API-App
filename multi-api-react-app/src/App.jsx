8582d3d1fb494ef10e7da6df21c56cab82630b74
lzalit238 <lzalit238@west-mec.org>
2021-10-11 14:05:07 -0700

    Update App.jsx

--- multi-api-react-app/src/App.jsx
+++ multi-api-react-app/src/App.jsx
@@ -1,31 +1,42 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import ParksCards from './Components/ParksCards';
 import './Styles/main.css'
 
 function App() {
-  const [parks, setParks] = useState([])
+  const [parks, setParks] = useState([]);
 
-  const axios = require('axios').default
-    axios.get('https://developer.nps.gov/api/v1/parks?&api_key=vcEt1EQ8P1p32S4YDuRlHUhUFtuNvYyew2t2xnnJ').then((response) => {
-      console.log(response);
-      const list = response.json();
-      setParks(list);
-      console.log(parks);
-    }).catch((error) => {
-    console.log(error)
-    })//call api
+  useEffect(() => {
+    getData();
+
+    async function getData() {
+      const response = await fetch('https://developer.nps.gov/api/v1/parks?&api_key=vcEt1EQ8P1p32S4YDuRlHUhUFtuNvYyew2t2xnnJ');
+      const data = await response.json();
+
+      setParks(data);
+    }
+  }, []);
+
+  // const axios = require('axios').default
+  //   axios.get('https://developer.nps.gov/api/v1/parks?&api_key=vcEt1EQ8P1p32S4YDuRlHUhUFtuNvYyew2t2xnnJ').then((response) => {
+  //     console.log(response);
+  //     const list = response.json();
+  //     setParks(list);
+  //     console.log(parks);
+  //   }).catch((error) => {
+  //   console.log(error)
+  //   })//call api
   
   
 
   return (
     <div className="App">
       <h1 className='title'>Parks to Explore!</h1>
-      {parks.map((places) => {
-        return (
-          <ParksCards parks={places} />
-        )
-      })
-      }
+      {parks.map((parks, id) => (
+        <div key={id}>
+          <h2>{parks.fullName}</h2>
+        </div>
+      )
+      )}
     </div>
   );
 }
